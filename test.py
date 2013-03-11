0fc6041267dfa72ef623632e955321e72797a079
it <it@raspi1237.olin.edu>
2013-03-10 23:35:17 -0400

    clean up transmission code, add CRC functionality, add abstractions

--- test.py
+++ test.py
@@ -3,30 +3,36 @@ from ctypes import c_ubyte, c_int
 
 import socket
 import time
+import crc
+
 r = ctypes.PyDLL("./radio.so")
 r.setup_io()
-
-# from someone else
-div14464 = 0x374F
 
 div = lambda freq: c_int(int((1<<12)*500e6/freq))
 
 r.setup_fm(div(144.64e6))
 
+triggerSocket = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
+triggerSocket.bind ( ( '10.33.91.11', 9000) )
+triggerSocket.listen ( 1 )
 
-mySocket = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
-mySocket.bind ( ( '10.33.91.11', 80) )
-mySocket.listen ( 1 )
+def hexToByte( hexStr ):
+    bytes = []
+    hexStr = ''.join( hexStr.split(" ") )
+    for i in range(0, len(hexStr), 2):
+        bytes.append( chr( int (hexStr[i:i+2], 16 ) ) )
+    return ''.join( bytes )
 
+def sendMessage(message = "hello there, world!", sym = chr(104)):
+	crcMsg = crc.crc32(message)|1<<33
+	crcStr = hexToByte(hex(crcMsg)[3::][0:-1])
+	r.sendStringAsk(message+crcStr, ord(sym))
+	print [hex(ord(x))[2::] for x in message+crcStr]
 
 while True:
 	r.askLow()
-	channel, details = mySocket.accept()
-	channel.recv(1)
+	channel, details = triggerSocket.accept()
+	sym = channel.recv(1)
 	time.sleep(0.05)
-	r.sendStringAsk("hello world", 1000)
-	#for letter in "hello world":
-	#	# byte, mark
-	#	r.sendByteAsk(c_ubyte(ord(letter)), c_int(1000))
-	#	print letter, hex(ord(letter))
+	sendMessage("hello world", sym)
 	channel.close()
