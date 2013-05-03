ccfec5c4fdc98f7cda4d7b50e695eb61ac7cdea4
it <it@raspi1237.olin.edu>
2013-05-03 17:16:22 -0400

    added initial commit of crc32, ported from rosetta code's 'go' example

---
+++ crc.py
@@ -0,0 +1,19 @@
+def genTable():
+	table = []
+	for i in range(256):
+		word = i
+		for j in range(8):
+			if word&1 == 1:
+				word = (word >> 1) ^ 0xedb88320
+			else:
+				word >>= 1
+		table.append(word)
+	return table
+		
+
+def crc32(string, crc=0xFFFFFFFF, table = genTable()):
+	for i in range(len(string)):
+		crc = table[(crc&0xFF)^ord(string[i])] ^ (crc >> 8)
+	return 0xFFFFFFFF^crc
+
+print hex(crc32("The quick brown fox jumps over the lazy dog"))
